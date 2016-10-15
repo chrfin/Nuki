f11885a763626a7536f296413a0a18455af084f6
Christoph Fink <fink.christoph@gmail.com>
2016-10-15 15:16:07 +0200

    minor fixes/improvements

--- NukiAPI/NukiAPI/WebAPI/LockState.cs
+++ NukiAPI/NukiAPI/WebAPI/LockState.cs
@@ -10,11 +10,11 @@ namespace NukiAPI.WebAPI
     {
         Uncalibrated = 0,
         Locked = 1,
-        UnlockedAndGo = 2,
+        Unlocking = 2,
         Unlocked = 3,
-        Unlatched = 4,
-        Locking = 5,
-        Unlocking = 6,
+        Locking = 4,
+        Unlatched = 5,
+        UnlockedAndGo = 6,
         Unlatching = 7,
         MotorBlocked = 254,
         Undefined = 255
